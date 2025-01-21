caf93bcc1e2a7f83b4aaf6187acdfc62cf05752e
Lebedev Light <LightlessOne@users.noreply.github.com>
2025-01-21 14:10:27 +0300

    Add integration tests (#10)

---
+++ .github/scripts/setup_minio.sh
@@ -0,0 +1,5 @@
+set -e
+wget -q https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x ./mc
+./mc alias set minio http://127.0.0.1:9000 $MINIO_USER $MINIO_PASSWORD
+./mc mb --ignore-existing minio/integration-test-bucket
