d33efd89bf1f3dff8e725efb3c3bcb37c2bdfefc
Igor Lebedev <lebedev.light@gmail.com>
2025-11-25 17:59:39 +0300

    feat: Added GitHub Run Pipeline command

--- tests/v2/podman/test_podman_command.py
+++ tests/v2/podman/test_podman_command.py
@@ -4,19 +4,18 @@ from qubership_pipelines_common_library.v2.podman.podman_command import PodmanRu
 
 class TestPodmanRunImage:
 
-    def test_podman_run_fails_without_image(self, caplog):
+    def test_podman_run_fails_without_image(self, caplog, tmp_path):
         with pytest.raises(SystemExit) as exit_result:
-            cmd = PodmanRunImage(folder_path='dynamic_test_context_dir')
+            cmd = PodmanRunImage(folder_path=str(tmp_path))
             cmd.run()
 
         assert exit_result.value.code == 1
         assert "Parameter 'params.image' is mandatory" in caplog.text
 
-    def test_podman_run_fails_without_binaries(self, caplog):
+    def test_podman_run_fails_without_binaries(self, caplog, tmp_path):
         with pytest.raises(SystemExit) as exit_result:
-            cmd = PodmanRunImage(folder_path='dynamic_test_context_dir', input_params={'params':{'image':'qubership_non_existent_image:latest'}})
+            cmd = PodmanRunImage(folder_path=str(tmp_path), input_params={'params': {'image': 'qubership_non_existent_image:latest'}})
             cmd.run()
 
         assert exit_result.value.code == 1
-        # Initially planned to test absence of 'podman', but GitHub runner does have it
         assert "StatusCode: 404" in caplog.text
